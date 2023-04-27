e04e97c51c65070e44fd1c94107cf3050d912489
StevenPH1998 <125024442+StevenPH1998@users.noreply.github.com>
2023-04-27 23:55:06 +0800

    Delete login-signup directory

--- login-signup/js/torun.js
+++
@@ -1,8 +0,0 @@
-function run() {
-    var rainyDay = new RainyDay({
-        image: 'background' // Target p element with ID, RainyDay.js will use backgorund image to simulate rain effects.
-    });
-    window.setTimeout(function () {
-        // rainyDay.destroy()
-    }, 1000)
-}
